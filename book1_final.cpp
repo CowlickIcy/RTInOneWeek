cbc99d04ceb15b96d93dc071ca968703de3198e3
cowlick <2534905725@qq.com>
2021-09-11 21:22:26 +0800

    Add material description

--- book1_final.cpp
+++ book1_final.cpp
@@ -15,18 +15,23 @@
 color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
 
-    // If we've exceeded the ray bounce limit, no more light is gathered.
+    // Recursion exit.
     if (depth <= 0)
         return color(0,0,0);
 
     if (world.hit(r, 0.001, infinity, rec)) {
         ray scattered;
         color attenuation;
+
+        // For Lambertian, Dielectric, "scatter" always be true.
+        // For Metal, when normal point to the center, "scatter" will be false.
+
         if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
             return attenuation * ray_color(scattered, world, depth-1);
-        return color(0,0,0);
+        return color(0,0,0);                
     }
 
+    // If unhit, generate the background.
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
